055b8a564feca3c46449b821abffc327df915a18
Arnaud Poletto <arnaud.poletto@epfl.ch>
2022-12-18 02:45:12 +0100

    remove scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,47 +17,23 @@ function replayVideo() {
 
 // At the beginning
 window.onload = function() {
-    console.log('a')
     window.scroll(0, 0);
 }
 
 // Time bar
 window.onscroll = function() {
-    unscrollStart();
-    unscrollEnd();
     scrollFunction();
 };
 
 // The time-bar is a div that is 100% width of the screen
 // Becomes red when scroling down by percentage
 function scrollFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop - document.documentElement.clientHeight;
-    var height = document.documentElement.scrollHeight - 2 * document.documentElement.clientHeight;
-    var scrolled = Math.max(0, (winScroll / height) * 100);
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
     console.log(scrolled)
     n_px_dot = 20 - 40 * scrolled / 100;
     n_px_bar = 40 * scrolled / 100;
     document.getElementById("time-bar-full").style.width = "calc(" + scrolled + "% - " + n_px_bar + "px)";
     document.getElementById("time-dot").style.left = "calc(" + scrolled + "% + " + n_px_dot + "px)";
-}
-
-function unscrollStart() {
-    var height = window.innerHeight;
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    if(winScroll < height && !started_video) {
-        window.scroll(0, 0);
-    }
-
-    if(winScroll < height && started_video) {
-        window.scroll(0, height);
-    }
-}
-
-function unscrollEnd() {
-    var total_height = document.documentElement.scrollHeight
-    var height = total_height - 1.1 * document.documentElement.clientHeight;
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    if(winScroll > height && !ended_video) {
-        window.scroll(0, total_height);
-    }
 }
